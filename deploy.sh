0e046145cd36585a0fe7c8517b7122485f4cccda
Ken Fukuyama <fukuyama@supersoftware.co.jp>
2017-02-28 19:26:20 +0900

    initial commit

---
+++ deploy.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -e
+
+cd $DEPLOY_SRC
+
+# retrieve latest
+echo "git pull or clone"
+if [ ! -d $DEPLOY_SRC/.git ]
+then
+  git clone $GIT_URL .
+else
+  git pull $GIT_URL
+fi
+
+echo "checkout target branch"
+git checkout $GIT_BRANCH
+
+# build static site
+echo "building with hugo"
+hugo
+
+# sync src with dest
+echo "rsync $@"
+rsync "$@"
